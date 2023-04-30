39a77459003e578269a51f637fa0941a90b26b36
darwin <dazhurgen@gmail.com>
2023-04-30 17:42:44 -0400

    updated vuetify dependencies

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 
 
 // Vuetify
-//import 'vuetify/styles'
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
